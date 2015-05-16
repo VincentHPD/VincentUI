b4b94aeabb7d80896d733225d99f0d1e0d8504c1
Fabian Buentello <fbuentello@mioty.com>
2015-05-16 16:58:53 -0500

    add map

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,45 @@
 /**
 * myApp Module
 *
-* Fabians Personal Website
+* Vincent Website
 */
 app = angular.module('myApp', ['ngRoute','ngSanitize']);
-app.controller('mainController', ['$scope', function($scope){
+app.controller('mainController', ['$scope', function ($scope) {
+
+    var mapOptions = {
+        zoom: 18,
+        center: new google.maps.LatLng(29.7604, -95.3698),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $scope.markers = [];
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    var createMarker = function (info){
+
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.city
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.open($scope.map, marker);
+        });
+
+        $scope.markers.push(marker);
+
+    }
+
+    $scope.openInfoWindow = function(e, selectedMarker){
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    }
 
 }]);
 
