7f9e76fbd70efe525117534083d5b8c43cfb858b
Fabian Buentello <fbuentello@mioty.com>
2015-05-17 08:16:24 -0500

    add angular cdns, and disable sidebar with right click

--- app/js/app.js
+++ app/js/app.js
@@ -3,24 +3,25 @@
  *
  * Vincent Website
  */
-app = angular.module('myApp', ['ngRoute', 'ngSanitize']);
-app.controller('mainController', ['$scope', '$http', '$q', function($scope, $http, $q) {
-    $scope.murders = $scope.assaults = $scope.rapes = [];
-    $scope.beatName = "";
-    $scope.week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    var data = [];
-    var mapOptions = {
-        zoom: 12,
-        center: new google.maps.LatLng(29.7604, -95.3698),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+ app = angular.module('myApp', ['ngRoute']);
+ app.controller('mainController', ['$scope', '$http', '$q', function($scope, $http, $q) {
+
+ 	$scope.beatName = "";
+ 	$scope.enableSideBar = true;
+ 	$scope.week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+ 	var data = [];
+ 	var mapOptions = {
+ 		zoom: 12,
+ 		center: new google.maps.LatLng(29.7604, -95.3698),
+ 		mapTypeId: google.maps.MapTypeId.ROADMAP
+ 	}
 
 
 
-    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    /* Setup InputField*/
-    var inputDiv = document.getElementById('searchDiv');
-    var input = document.getElementById('pac-input');
+ 	$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+ 	/* Setup InputField*/
+ 	var inputDiv = document.getElementById('searchDiv');
+ 	var input = document.getElementById('pac-input');
 
     // Set InputField inside of Map
     $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputDiv);
@@ -44,20 +45,20 @@ app.controller('mainController', ['$scope', '$http', '$q', function($scope, $htt
 
             // Set image if important
             var image = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
+            	url: place.icon,
+            	size: new google.maps.Size(71, 71),
+            	origin: new google.maps.Point(0, 0),
+            	anchor: new google.maps.Point(17, 34),
+            	scaledSize: new google.maps.Size(25, 25)
             };
 
             // Set Marker
             var marker = new google.maps.Marker({
-                map: $scope.map,
-                icon: image,
-                animation: google.maps.Animation.DROP,
-                title: place.name || input.value,
-                position: place.geometry.location
+            	map: $scope.map,
+            	icon: image,
+            	animation: google.maps.Animation.DROP,
+            	title: place.name || input.value,
+            	position: place.geometry.location
             });
 
             // Add marker to markers Array
@@ -68,12 +69,12 @@ app.controller('mainController', ['$scope', '$http', '$q', function($scope, $htt
 
             // Add Listener to marker
             google.maps.event.addListener(marker, 'click', function() {
-                infoWindow = InfoWindow(marker.title);
-                infoWindow.open($scope.map, marker);
+            	infoWindow = InfoWindow(marker.title);
+            	infoWindow.open($scope.map, marker);
 
                 // Close Marker after 3 seconds
                 setTimeout(function() {
-                    infoWindow.close();
+                	infoWindow.close();
                 }, 3000);
             });
         }
@@ -83,67 +84,82 @@ app.controller('mainController', ['$scope', '$http', '$q', function($scope, $htt
     });
     //Add styles to Polygons
     $scope.map.data.setStyle(function(feature) {
-        var fill = feature.getProperty('fill');
-        var stroke = feature.getProperty('stroke');
-        return {
-            fillColor: '#FF0000',
-            strokeColor: stroke
-        };
+    	var fill = feature.getProperty('fill');
+    	var stroke = feature.getProperty('stroke');
+    	return {
+    		fillColor: '#FF0000',
+    		strokeColor: stroke
+    	};
     });
 
     $scope.map.data.addListener('mouseover', function(event) {
 
 
-        $scope.$apply(function() {
-            $scope.openSB = true;
-            getData();
-            beat = event.feature.A.name.toUpperCase();
-            $scope.beatName = beat;
-            $scope.crimes = getBeatData(beat);
-        });
+    	$scope.$apply(function() {
+    		if($scope.enableSideBar) {
+    			$scope.openSB = true;
+    		}
 
-        $scope.map.data.overrideStyle(event.feature, {
-            strokeWeight: 2.0,
-            fillColor: 'green'
-        });
+    		getData();
+    		beat = event.feature.A.name.toUpperCase();
+    		$scope.beatName = beat;
+    		$scope.crimes = getBeatData(beat);
+    	});
+
+    	$scope.map.data.overrideStyle(event.feature, {
+    		strokeWeight: 2.0,
+    		fillColor: 'green'
+    	});
     });
 
     $scope.map.data.addListener('mouseout', function(event) {
-        $scope.map.data.overrideStyle(event.feature, {
-            fillColor: '#FF0000'
-        });
+    	$scope.map.data.overrideStyle(event.feature, {
+    		fillColor: '#FF0000'
+    	});
+    });
+
+    $scope.map.data.addListener('rightclick', function(event) {
+    	$scope.$apply(function() {
+    		if ($scope.enableSideBar) {
+    			$scope.openSB = false;
+    			$scope.enableSideBar = false;
+    		} else {
+    			$scope.enableSideBar = true;
+    		}
+    	});
+
+
     });
 
     //display beat information
     $scope.map.data.addListener('click', function(event) {
     	$scope.$apply(function() {
-            $scope.openSB = false;
-        });
+    		$scope.openSB = false;
+    	});
     });
 
     var getData = function() {
-        var deferred = $q.defer();
-        if (data.length) {
-            deferred.resolve(data);
-        } else {
-            $http.get('js/future.json').success(function(d) {
-                data = d;
-                deferred.resolve(data);
-                console.log(data);
-            });
-        }
-        return deferred.promise;
+    	var deferred = $q.defer();
+    	if (data.length) {
+    		deferred.resolve(data);
+    	} else {
+    		$http.get('js/future.json').success(function(d) {
+    			data = d;
+    			deferred.resolve(data);
+    		});
+    	}
+    	return deferred.promise;
     };
     var getBeatData = function(_beat) {
-        return data[_beat];
+    	return data[_beat];
     };
 
     ///* Setup InfoWindow */
     var InfoWindow = function(content) {
         //Create InfoWindow
         var iWindow = new google.maps.InfoWindow({
-            content: 'The title of this marker is ' + content + 'a maxWidth of 100 and it closes in 3 seconds',
-            maxWidth: 100
+        	content: 'The title of this marker is ' + content + 'a maxWidth of 100 and it closes in 3 seconds',
+        	maxWidth: 100
         });
 
         return iWindow;
@@ -153,9 +169,9 @@ app.controller('mainController', ['$scope', '$http', '$q', function($scope, $htt
 }]);
 
 app.directive('sideBar', function() {
-    return {
-        restrict: 'E',
-        controller: 'mainController',
-        templateUrl: './partials/sideBar.tpl.html'
-    };
+	return {
+		restrict: 'E',
+		controller: 'mainController',
+		templateUrl: './partials/sideBar.tpl.html'
+	};
 });
