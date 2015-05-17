5a171a75e67f684d76a0661d0a3a29849e012ab1
Fabian Buentello <fbuentello@mioty.com>
2015-05-16 19:22:37 -0500

    add styles to poloygons

--- app/js/app.js
+++ app/js/app.js
@@ -26,63 +26,86 @@ app.controller('mainController', ['$scope', function ($scope) {
 
 	google.maps.event.addListener(searchBox, 'places_changed', function() {
 
-            // Get Coordinates
-            var places = searchBox.getPlaces();
+		// Get Coordinates
+		var places = searchBox.getPlaces();
 
-            if (places.length == 0) return;
+		if (places.length == 0) return;
 
-            // Clear out markers Array
-            markers = [];
+		// Clear out markers Array
+		markers = [];
 
-            var bounds = new google.maps.LatLngBounds();
-            for (var i = 0, place; place = places[i]; i++) {
+		var bounds = new google.maps.LatLngBounds();
+		for (var i = 0, place; place = places[i]; i++) {
 
-                // Set image if important
-                var image = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25)
-                };
+			// Set image if important
+			var image = {
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(25, 25)
+			};
 
-                // Set Marker
-                var marker = new google.maps.Marker({
-                    map: $scope.map,
-                    icon: image,
-                    animation: google.maps.Animation.DROP,
-                    title: place.name || input.value,
-                    position: place.geometry.location
-                });
+			// Set Marker
+			var marker = new google.maps.Marker({
+				map: $scope.map,
+				icon: image,
+				animation: google.maps.Animation.DROP,
+				title: place.name || input.value,
+				position: place.geometry.location
+			});
 
-                // Add marker to markers Array
-                markers.push(marker);
+			// Add marker to markers Array
+			markers.push(marker);
 
-                // Change bounds of Map to marker
-                bounds.extend(place.geometry.location);
+			// Change bounds of Map to marker
+			bounds.extend(place.geometry.location);
 
-                // Add Listener to marker
-                google.maps.event.addListener(marker, 'click', function() {
-                    infoWindow = InfoWindow(marker.title);
-                    infoWindow.open($scope.map, marker);
+			// Add Listener to marker
+			google.maps.event.addListener(marker, 'click', function() {
+				infoWindow = InfoWindow(marker.title);
+				infoWindow.open($scope.map, marker);
 
-                    // Close Marker after 3 seconds
-                    setTimeout(function() {
-                        infoWindow.close();
-                    }, 3000);
-                });
-            }
+			// Close Marker after 3 seconds
+			setTimeout(function() {
+				infoWindow.close();
+			}, 3000);
+		});
+		}
 
-            // Update map's bounds
-            $scope.map.fitBounds(bounds);
-        });
+		// Update map's bounds
+		$scope.map.fitBounds(bounds);
+	});
+
+$scope.map.data.setStyle(function(feature) {
+	var fill = feature.getProperty('fill');
+	var stroke = feature.getProperty('stroke');
+	return {
+		fillColor: '#FF0000',
+		strokeColor: stroke
+	};
+});
+	//Add styles to Polygons
+	$scope.map.data.addListener('mouseover', function(event) {
+		$scope.map.data.overrideStyle(event.feature, {
+			strokeWeight: 2.0,
+			fillColor: 'green'
+		});
+	});
+
+	$scope.map.data.addListener('mouseout', function(event) {
+		$scope.map.data.overrideStyle(event.feature, {
+			fillColor: '#FF0000'
+		});
+	});
+
 
 	///* Setup InfoWindow */
-    var InfoWindow = function(content) {
+	var InfoWindow = function(content) {
         //Create InfoWindow
         var iWindow = new google.maps.InfoWindow({
-            content: 'The title of this marker is ' + content+ 'a maxWidth of 100 and it closes in 3 seconds',
-            maxWidth: 100
+        	content: 'The title of this marker is ' + content+ 'a maxWidth of 100 and it closes in 3 seconds',
+        	maxWidth: 100
         });
 
         return iWindow;
