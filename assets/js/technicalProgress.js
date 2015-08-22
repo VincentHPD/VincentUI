2cb101459b3efac85dec0968c7b4ac3c486ed844
Fabian Buentello <fbuentello@mioty.com>
2015-08-22 07:32:27 -0500

    add technical progress starter page

---
+++ assets/js/technicalProgress.js
@@ -0,0 +1,30 @@
+
+
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
+
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+	$.get("../../Technical-Progress/technicalProgress.json", function(jsonData, status){
+
+		// Create the data table.
+		var dataTb = new google.visualization.DataTable(jsonData);
+
+		// Set chart options
+		var options = {
+			'title':'Vincent\'s Progress over time.',
+			'tooltip': {isHtml: true}
+		};
+
+		// Instantiate and draw our chart, passing in some options.
+		var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+		chart.draw(dataTb, options);
+	});
+}
